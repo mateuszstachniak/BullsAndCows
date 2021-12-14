f3cd167987db39ab7d37ff7fcb3ea939c5df6ef7
mateuszstachniak <91155660+mateuszstachniak@users.noreply.github.com>
2021-12-14 02:09:16 +0100

    Add pseudo number generator

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 # Assumption
 
-Its a stage 2 of JetBrains Academy project. The grader is ready for check the game result, and print it for a user.
+It's a stage 3 of JetBrains Academy project. The grader is ready for check the game result, and prints it for a user. Also pseudo random number generator was implemented. /
 
+It generates secret code of the given length with unique numbers.
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String secretDigits = "9004";
-        String userDigitsInput = scanner.nextLine();
+        StringBuilder secretDigits = new StringBuilder();
+        int n = scanner.nextInt();
 
-        cowsBulls(secretDigits, userDigitsInput);
+        if (n > 10) {
+            errorMessage();
+        } else {
+            pseudoRandomNumberGenerator(n, secretDigits);
+            System.out.println(secretDigits);
+        }
+
+
+        /*        String userDigitsInput = scanner.nextLine();*/
+        /*        cowsBulls(secretDigits, userDigitsInput);*/
     }
 
-    public static void cowsBulls(String secret, String userDigits) {
+/*    public static void cowsBulls(String secret, String userDigits) {
         int cows = 0;
         int bulls = 0;
         for (int i = 0; i < secret.length(); i++) {
@@ -36,6 +45,43 @@ public class Main {
         } else {
             System.out.println("Grade: None. The secret code is " + secret + ".");
         }
+    }*/
+
+    public static void errorMessage() {
+        System.out.println("Error");
+    }
+
+    public static StringBuilder pseudoRandomNumberGenerator(int n, StringBuilder secretDigits) {
+        boolean repeat = true;
+        do {
+            String pseudoRandomNumber = Long.toString(System.nanoTime()).substring(0, 11);
+
+            for (int i = 1; i <= n; i++) {
+                secretDigits.append(pseudoRandomNumber.charAt(pseudoRandomNumber.length() - i));
+            }
+
+            for (int i = 0; i < secretDigits.length(); i++) {
+                for (int j = 0; j < secretDigits.length(); j++) {
+                    if (secretDigits.charAt(0) != '0' && i == j && n == 1) {
+                        repeat = false;
+                    } else if (i == j) {
+
+                    } else if (secretDigits.charAt(0) == '0') {
+                        repeat = true;
+                        secretDigits.setLength(0);
+                        break;
+                    } else if (secretDigits.charAt(i) == secretDigits.charAt(j)) {
+                        repeat = true;
+                        secretDigits.setLength(0);
+                        break;
+                    } else {
+                        repeat = false;
+                    }
+                }
+            }
+
+        } while (repeat);
+        return secretDigits;
     }
 
 }
